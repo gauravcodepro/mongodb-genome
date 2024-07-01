7cc0bbb25536707ce5150d2310d0d8d763d381e2
Gaurav <gaurav.sablok@uni-potsdam.de>
2024-07-01 07:31:09 +0200

    adding dataclasses for faster import with static type 2024-7-1

--- mongodb-gff.py
+++ mongodb-gff.py
@@ -2,49 +2,44 @@
 # Author Gaurav
 # Universitat Potsdam
 # Date 2024-6-24
+# a mongodb class to prepare the genome annotations for the genomedb, you can prepare any gff file for the
+# pymongodb as long as it meets the same gff pattern. since the genome annotations id will be duplicated ,
+# implmented a list based nested iteration so that it can be inserted into mongodb as .insertMany()
 
 import os
 import pandas as pd
 
-
 class MongoDB:
+    
 
     def mongodbprepare(gfffile, prepare):
-        """
-     a mongodb class to prepare the genome annotations
-     for the genomedb, you can prepare any gff file for the
-     pymongodb as long as it meets the same gff pattern.
-     since the genome annotations id will be duplicated ,
-     implmented a list based nested iteration so that it can
-     be inserted into mongodb as .insertMany()
-        """
-    if gfffile and prepare == "yes":
-        with open(gfffile, "r") as gffread:
-            with open("gfffilemod", "w") as gffwrite:
-                gffwrite.write("column1" + "\t" + "column2" + "\t" +"column3" + "\t" +
-                  "column4" + "\t" + "column5" + "\t" + "column6" + "\t" + "column7" +
-                  "\t" + "column8" + "\t" + "column9\n")
+        if gfffile and prepare == "yes":
+            with open(gfffile, "r") as gffread:
+                with open("gfffilemod", "w") as gffwrite:
+                    gffwrite.write("column1" + "\t" + "column2" + "\t" +"column3" + "\t" +
+                    "column4" + "\t" + "column5" + "\t" + "column6" + "\t" + "column7" +
+                    "\t" + "column8" + "\t" + "column9\n")
                 for line in gffread.readlines():
                     gffwrite.write(line)
                 gffwrite.close()
-    gffdataframe = pd.read_csv("gfffilemod", sep = "\t")
-    genomeloc = gffdataframe["column1"].to_list()
-    accession = gffdataframe["column2"].to_list()
-    typeaccession = gffdataframe["column3"].to_list()
-    start = gffdataframe["column4"].to_list()
-    end = gffdataframe["column5"].to_list()
-    length = gffdataframe["column6"].to_list()
-    posstrand = gffdataframe["column7"].to_list()
-    negstrand = gffdataframe["column8"].to_list()
-    idlocation = gffdataframe["column9"].to_list()
-    mongodbprepare = []
-    for i in range(len(genomeloc)):
+        gffdataframe = pd.read_csv("gfffilemod", sep = "\t")
+        genomeloc = gffdataframe["column1"].to_list()
+        accession = gffdataframe["column2"].to_list()
+        typeaccession = gffdataframe["column3"].to_list()
+        start = gffdataframe["column4"].to_list()
+        end = gffdataframe["column5"].to_list()
+        length = gffdataframe["column6"].to_list()
+        posstrand = gffdataframe["column7"].to_list()
+        negstrand = gffdataframe["column8"].to_list()
+        idlocation = gffdataframe["column9"].to_list()
+        mongodbprepare = []
         for i in range(len(genomeloc)):
-            mongodbprepare[genomeloc[i]] = [{genomeloc[i]: accession[i]}, {genomeloc[i]: typeaccession[i]},
+            for i in range(len(genomeloc)):
+                mongodbprepare[genomeloc[i]] = [{genomeloc[i]: accession[i]}, {genomeloc[i]: typeaccession[i]},
                                         {genomeloc[i]: start[i]}, {genomeloc[i]:end[i]},
                                         {genomeloc[i]:length[i]}, {genomeloc[i]: posstrand[i]},
                                         {genomeloc[i]:negstrand[i]}, {genomeloc[i]: idlocation[i]}]dlocation[i]}])
-    return mongodbprepare
+        return mongodbprepare
 
     def fastaindex(pathfasta, mongopreparefasta):
         if pathfasta and monopreparefasta == "yes":
